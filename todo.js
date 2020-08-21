dd3cb8d170a0294917a4ed68888de595803db11a
AP <porwal.1@iitj.ac.in>
2020-08-21 15:11:41 +0530

    Note title feature added

--- todo.js
+++ todo.js
@@ -3,13 +3,13 @@ window.onload = () => {
     display_all_notes();
 }
 
-
+// let titles = [];
 
 //Function for Showing all Notes in WEBPAGE
-all_notes_in_document = (value, key) => {
+all_notes_in_document = (tt, value, key) => {
     document.getElementById("my-all-notes").innerHTML += `<div class="noteCard card mt-2 mb-4 mr-4" style="width: 21rem;height:auto" id=${key}>
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">${tt}</h5>
             <p class="card-text">${value}</p>
             <a href="#" class="btn btn-primary" id="delete_note" onclick=delete_note(${key})>Delete Note</a>
         </div>
@@ -24,10 +24,11 @@ display_all_notes = () => {
     //All Values from Localstorage
     let items = Object.values(localStorage);
     let keys = Object.keys(localStorage);
-
     if (items.length > 0) {
         for (let i = 0; i < items.length; i++) {
-            all_notes_in_document(items[i], keys[i]);
+            let perfect_item = JSON.parse(String(items[i]));
+            // console.log(perfect_item)
+            all_notes_in_document(perfect_item.title, perfect_item.text, keys[i]);
         }
     } else {
 
@@ -43,20 +44,24 @@ let count = Object.keys(localStorage).sort().reverse()[0] === undefined ? 0 : Nu
 //Adding a note
 document.getElementById("add_note").addEventListener("click", () => {
     let text = document.getElementById("text_area").value;
+    let note_title = document.getElementById("note_title").value;
     if (Object.keys(localStorage).indexOf(String(count)) == -1) {
 
-
-        localStorage.setItem(String(count), text);
+        // titles.push(note_title);
+        localStorage.setItem(String(count), JSON.stringify({ title: note_title, text: text }));
         document.getElementById("text_area").value = "";
+        document.getElementById("note_title").value = "";
 
         if (document.getElementById("my-all-notes").innerHTML == `<h5 class="mt-4">Nothing to show! Use "Add Your Notes Here" section to add notes.</h5>`) {
 
             document.getElementById("my-all-notes").innerHTML = "";
-            all_notes_in_document(text, count)
+            // all_notes_in_document(text, count)
+            display_all_notes();
 
         } else {
 
-            all_notes_in_document(text, count);
+            // all_notes_in_document(text, count);
+            display_all_notes();
 
         }
 
@@ -99,3 +104,6 @@ document.getElementById("search").addEventListener("input", () => {
     });
 
 })
+
+
+// console.log(titles)
